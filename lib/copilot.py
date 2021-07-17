8f27ef4ef6ced26933b16e2bd1901e573383bb3a
Pranav Kasetti <pranavkasetti@gmail.com>
2021-07-17 10:37:39 +0100

    Add tests with copilot for Python.

---
+++ lib/copilot.py
@@ -0,0 +1,7 @@
+# Generate random number
+import random
+# Random number from 1 to 10
+rand = random.randint(1, 10)
+# Ask user to guess
+guess = input("Guess a number between 1 and 10: ")
+# If guess is not equal to random number
